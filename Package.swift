70c2288f4493be56d9aa428290dca9057a7d8f06
Wojciech Sierotowicz <wojexe@gmail.com>
2023-11-07 23:34:33 +0100

    Generate Vapor project.

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "genealogy_backend",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        // 🪶 Fluent driver for SQLite.
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+
+            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Fluent", package: "Fluent"),
+            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+        ])
+    ]
+)
